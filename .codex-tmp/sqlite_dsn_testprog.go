7db07d1580ac7e2189ff40ec2907000025282f9c
Dumitru Vulpe <dumitru.vulpe@outlook.com>
2026-04-27 10:24:08 +0100

    chore: cleanup

--- .codex-tmp/sqlite_dsn_testprog.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-  "fmt"
-  hsqlite "github.com/FyrmForge/hamr/pkg/db/sqlite"
-)
-func main(){
-  db,err:=hsqlite.Connect("file::memory:?cache=shared")
-  fmt.Printf("dbnil=%v err=%v\n", db==nil, err)
-}
